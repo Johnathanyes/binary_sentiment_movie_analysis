859e819ebafbfb98b7c290f7b0a862e205b2b6dd
Johnathan Lee <johnathanYLee327@gmail.com>
2025-04-10 15:34:18 -0400

    working on lemmatization

--- functions.py
+++ functions.py
@@ -2,6 +2,6 @@ import string
 string.punctuation
 
 def remove_punctuation_and_lower(text: str):
-    punctuation_free = "".join([i for i in text if i not in string.puntuation])
+    punctuation_free = "".join([i for i in text if i not in string.punctuation])
     lower = punctuation_free.lower()
     return lower
